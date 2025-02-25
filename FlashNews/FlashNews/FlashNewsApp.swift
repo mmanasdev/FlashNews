67d2cc7cc96dcbd5cac7bee6aa7f9c5bed7db972
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2025-02-25 20:17:11 +0100

    feat/initial-project: create ios app project

---
+++ FlashNews/FlashNews/FlashNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashNewsApp.swift
+//  FlashNews
+//
+//  Created by Miguel Mañas Alvarez on 25/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
